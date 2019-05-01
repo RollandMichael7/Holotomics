f91ec02ae6a51a39a07640c4db9a0cb50a99cc2d
Michael Rolland <rollandmichael7@gmail.com>
2019-05-01 18:31:16 -0400

    Add periodic table screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+ 
+ public class SceneLoader: MonoBehaviour {
+ 
+      public void LoadScene(int level)
+      { 
+         Application.LoadLevel(level);
+       }
+ }
