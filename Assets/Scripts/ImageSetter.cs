7ecdb75379f7629a9a838dde85585f40898c5a56
Michael Rolland <rollandmichael7@gmail.com>
2019-05-05 19:51:26 -0400

    Add hologram screen

---
+++ Assets/Scripts/ImageSetter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSetter : MonoBehaviour
+{
+    private Image imageComponent;
+
+    void Start()
+    {
+        this.imageComponent = this.GetComponent<Image>();
+    }
+
+    public void setImage(string image)
+    {
+        this.imageComponent.sprite = Resources.Load<Sprite>("Holograms/" + image);
+    }
+}
