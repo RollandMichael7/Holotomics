88106952386f94c0434a74ce86235476e30486a7
Michael Rolland <rollandmichael7@gmail.com>
2019-05-05 21:38:10 -0400

    Add json compatibility for more elements and chemicals

--- Assets/Scripts/ElementInfo.cs
+++ Assets/Scripts/ElementInfo.cs
@@ -4,6 +4,29 @@
 public class ElementInfo
 {
     public string hydrogen;
+    public string helium;
+    public string lithium;
+    public string beryllium;
+    public string boron;
+    public string carbon;
+    public string nitrogen;
+    public string oxygen;
+    public string fluorine;
+    public string neon;
+    public string sodium;
+    public string magnesium;
+    public string aluminum;
+    public string silicon;
+    public string phosphorous;
+    public string sulfur;
+    public string chlorine;
+    public string argon;
+    public string potassium;
+    public string calcium;
+
+    public string water;
+    public string co2;
+    public string glucose;
 
     public static ElementInfo CreateFromJSON(string path)
     {
